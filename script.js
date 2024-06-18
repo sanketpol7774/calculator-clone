112d720e5059ea4ab9ce0cc65d9c21e96cb62dfd
SANKET POL <158428269+sanketpol7774@users.noreply.github.com>
2024-06-18 08:01:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let currentvalue = "";
+
+let buttun = document.querySelectorAll(".button-div button");
+
+
+
+
+
+
+// let sanket = function result();{
+
+//     eval(currentvalue);document.getElementById('divinput').value="sanket"
+
+// }
+
+
+
+
+
+//  let result = eval(currentvalue);document.getElementById('divinput').value=result"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
